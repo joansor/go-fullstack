9df6c19c6cfe0f6399371ef5cde8ea1a3fa2deb2
joansor <sor.joan@gmail.com>
2020-03-20 19:19:52 +0100

    add controller and router réorganisation

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+
 
 mongoose.connect('mongodb+srv://admin:root@cluster-0zvyu.gcp.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -10,49 +12,15 @@ mongoose.connect('mongodb+srv://admin:root@cluster-0zvyu.gcp.mongodb.net/test?re
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-const Thing = require('./models/thing');
-
-
-app.use((req, res, next) => {
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-  
+
   app.use(bodyParser.json());
 
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-  });
-  app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
+  app.use('/api/stuff',stuffRoutes);
 
-
-
-  
 module.exports = app;
