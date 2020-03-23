8ca855d726b5d20e53de834cb622c0e72d2ffdfa
joansor <sor.joan@gmail.com>
2020-03-23 18:39:49 +0100

    fin de l'exercice openclassroom

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
-
-
+const userRoutes = require('./routes/user');
+const path = require('path');
 mongoose.connect('mongodb+srv://admin:root@cluster-0zvyu.gcp.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -21,6 +21,10 @@ mongoose.connect('mongodb+srv://admin:root@cluster-0zvyu.gcp.mongodb.net/test?re
 
   app.use(bodyParser.json());
 
-  app.use('/api/stuff',stuffRoutes);
 
+  app.use('/api/stuff',stuffRoutes);
+  app.use('/api/stuff', stuffRoutes);
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use('/api/auth', userRoutes);
+  
 module.exports = app;
--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,12 +1,14 @@
 
 const Thing = require('../models/thing');
-
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  const thing = new Thing({
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
     thing.save()
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
       .catch(error => res.status(400).json({ error }));
@@ -19,17 +21,28 @@ exports.createThing = (req, res, next) => {
   };
 
   exports.modifyThing = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
   };
 
   exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => {
+        const filename = thing.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Thing.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
   };
-  
   exports.getAllStuff = (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
